5b79e63161be5b887750e11f5b73430554c3b407
Crowdbotics <team@crowdbotics.com>
2020-05-17 01:43:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tc_shaved_ice_17082/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tc_shaved_ice_17082;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "tc_shaved_ice_17082";
+    }
+}
